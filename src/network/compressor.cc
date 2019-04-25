7e15242aba50fb83407d0e8746b5184efef19b2d
Keegan McAllister <mcallister.keegan@gmail.com>
2019-04-24 19:06:29 -0700

    Don't compress

--- src/network/compressor.cc
+++ src/network/compressor.cc
@@ -40,20 +40,12 @@ using std::string;
 
 string Compressor::compress_str( const string &input )
 {
-  long unsigned int len = BUFFER_SIZE;
-  dos_assert( Z_OK == compress( buffer, &len,
-				reinterpret_cast<const unsigned char *>( input.data() ),
-				input.size() ) );
-  return string( reinterpret_cast<char *>( buffer ), len );
+  return string( input );
 }
 
 string Compressor::uncompress_str( const string &input )
 {
-  long unsigned int len = BUFFER_SIZE;
-  dos_assert( Z_OK == uncompress( buffer, &len,
-				  reinterpret_cast<const unsigned char *>( input.data() ),
-				  input.size() ) );
-  return string( reinterpret_cast<char *>( buffer ), len );
+  return string( input );
 }
 
 /* construct on first use */
